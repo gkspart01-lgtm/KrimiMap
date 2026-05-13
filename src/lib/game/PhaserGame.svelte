aad6dee6e0ed08b9483050a89c85a09fd89ee4c7
gkspart01-lgtm <gkspart01@gmail.com>
2026-05-13 06:08:15 +0200

    Add SvelteKit + Phaser Boilerplate

---
+++ src/lib/game/PhaserGame.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	import Phaser from 'phaser';
+	import MainScene from './MainScene';
+
+	let game: Phaser.Game | null = null;
+	let container: HTMLDivElement;
+
+	onMount(() => {
+		const config: Phaser.Types.Core.GameConfig = {
+			type: Phaser.AUTO,
+			parent: container,
+			width: 1200,
+			height: 800,
+			backgroundColor: '#2c2c2c',
+			scene: [MainScene],
+			physics: {
+				default: 'arcade',
+				arcade: { debug: false }
+			}
+		};
+
+		game = new Phaser.Game(config);
+	});
+
+	onDestroy(() => {
+		if (game) {
+			game.destroy(true);
+		}
+	});
+</script>
+
+<div bind:this={container} style="width: 100%; height: 100%;"></div>
